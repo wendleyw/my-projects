f607937f29da3e2cd39f0a0cd870377915f1302d
wendleyw <wendleywilson@gmail.com>
2025-04-13 14:23:14 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import WorkoutTracker from './components/WorkoutTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <WorkoutTracker />
+    </div>
+  );
+}
+
+export default App;
